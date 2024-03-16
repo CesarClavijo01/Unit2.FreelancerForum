d906ec2dc5b1fe0a1ac6cc43270e796766a7238a
CesarClavijo01 <cesar@Cesar>
2024-03-15 21:47:59 -0500

    updated workshop

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 //create an array of available freelancers
 const availableFreelancers = [
-    {name:'Alice',occupation:'Writer',price:30},
-    {name:'Bob',occupation:'Teacher',price:50},
-    {name:'Carol',occupation:'Programer',price:70}
-];
+    { name: 'Alice', occupation: 'Writer', price: 30 },
+    { name: 'Bob', occupation: 'Teacher', price: 50 },
+    { name: 'Carol', occupation: 'Programer', price: 70 },
+  ];
 
 //create a list of possible freelancers to add to available freelancer list
 const possibleFreelancers = [
-    { id: 1, name: "Dr. Slice", price: 25, occupation: "gardener" },
-    { id: 2, name: "Dr. Pressure", price: 51, occupation: "programmer" },
-    { id: 3, name: "Prof. Possibility", price: 43, occupation: "teacher" },
-    { id: 4, name: "Prof. Prism", price: 81, occupation: "teacher" },
-    { id: 5, name: "Dr. Impulse", price: 43, occupation: "teacher" },
-    { id: 6, name: "Prof. Spark", price: 76, occupation: "programmer" },
-    { id: 7, name: "Dr. Wire", price: 47, occupation: "teacher" },
-    { id: 8, name: "Prof. Goose", price: 72, occupation: "driver" },
+    { name: "Dr. Slice", price: 25, occupation: "gardener" },
+    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+    { name: "Prof. Prism", price: 81, occupation: "teacher" },
+    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "programmer" },
+    { name: "Dr. Wire", price: 47, occupation: "teacher" },
+    { name: "Prof. Goose", price: 72, occupation: "driver" },
   ];
 
+  let interval
 //create constant to select body
 const main = document.querySelector("main");
 
@@ -32,11 +33,11 @@ main.append(listTitle);
 
 
 //create a function that creates list for each object in an array
-function createListItems(list){
+function render(list){
     const freelancerList = document.createElement("div");
     const name = createName("Name:" + " " + list.name);
     const occupation = createOccupation("Occupation:" + " " + list.occupation);
-    const price = createPrice("Price:" + " " + list.price);
+    const price = createPrice("Price:" + " " + "$" + list.price);
 
     freelancerList.append(name);
     freelancerList.append(occupation);
@@ -76,10 +77,44 @@ function createPrice(price){
 }
 
 //run the function for each item in the array
-availableFreelancers.forEach((item) => createListItems(item))
-
-//create a function that pulls objects from the array and appends into list
+availableFreelancers.forEach((item) => render(item));
 
 //create a function that displays the average starting price of all the freelancers
+function priceAverage(){
+   const priceSum = availableFreelancers.reduce((acc, number) => acc + number.price, 0);
+   const averagePrice = priceSum / availableFreelancers.length;
+   return averagePrice;
+}
+//render priceaverage function
+const averageDiv = document.createElement("div");
+const averageline = document.createElement("p");
+const averageText = document.createTextNode(`The average price is:`+ " " + priceAverage());
+averageline.appendChild(averageText);
+averageDiv.appendChild(averageline);
+main.appendChild(averageDiv)
 
-//create a function that adds ne freelancers to the list every few seconds
+//create a function that adds new freelancers to the list every few seconds
+function addRandomFreelancer(){
+    //generate a random number
+    const randomNum = Math.floor(Math.random() * possibleFreelancers.length);
+
+    //splice the object with the random number from possibleFreelancers array
+    //find the random freelancer
+    const freelancer = possibleFreelancers[randomNum];
+
+    //find the index of the random freelancer
+    const index = possibleFreelancers.indexOf(freelancer);
+
+    //splice from possibleFreelancers
+    possibleFreelancers.splice(index, 1);
+
+    //push freelancer object into available freelancer list
+    availableFreelancers.push(freelancer);
+    
+    console.log()
+
+}
+
+//create an interval to add random objects to availableFreelancers
+
+interval = setInterval(addRandomFreelancer, 5000)
